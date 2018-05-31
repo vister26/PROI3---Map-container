a7967dfbfbac2a759b37ef3a466b66406ea5b33a
vister26 <visteral0ne@gmail.com>
2018-05-31 20:49:22 +0200

    RBTree test without templates
    first code from the internet

---
+++ RBTree.h
@@ -0,0 +1,32 @@
+#include<iostream>
+
+struct node
+{
+       int key;
+       node *parent;
+       char color;
+       node *left;
+       node *right;
+};
+
+class RBtree
+{
+      node *root;
+      node *q;
+   public :
+      RBtree()
+      {
+              q=NULL;
+              root=NULL;
+      }
+      void insert();
+      void insertfix(node *);
+      void leftrotate(node *);
+      void rightrotate(node *);
+      void del();
+      node* successor(node *);
+      void delfix(node *);
+      void disp();
+      void display( node *);
+      void search();
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
-#include <iostream>
-#include <map>
+#include "RBTree.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int ch,y=0;
+    RBtree obj;
+    do
+    {
+                cout<<"\n\t RED BLACK TREE " ;
+                cout<<"\n 1. Insert in the tree ";
+                cout<<"\n 2. Delete a node from the tree";
+                cout<<"\n 3. Search for an element in the tree";
+                cout<<"\n 4. Display the tree ";
+                cout<<"\n 5. Exit " ;
+                cout<<"\nEnter Your Choice: ";
+                cin>>ch;
+                switch(ch)
+                {
+                          case 1 : obj.insert();
+                                   cout<<"\nNode Inserted.\n";
+                                   break;
+                          case 2 : obj.del();
+                                   break;
+                          case 3 : obj.search();
+                                   break;
+                          case 4 : obj.disp();
+                                   break;
+                          case 5 : y=1;
+                                   break;
+                          default : cout<<"\nEnter a Valid Choice.";
+                }
+                cout<<endl;
+
+    }while(y!=1);
+    return 1;
 }
